10f67859d3dd76e27297c56640142f5331d7b101
Douglas Sergio <douglassergio95@hotmail.com>
2022-09-22 09:44:40 -0300

    Projeto Pronto

--- JavaScrip/PROJETOS/aluramidi/main.js
+++ JavaScrip/PROJETOS/aluramidi/main.js
@@ -1,4 +1,33 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+"use strict"
+
+function tocaSom(idElementoAudio) {
+    const elemento = document.querySelector(idElementoAudio);
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento nulo ou Elemento nao é um audio')
+    };
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;// template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+
+    tecla.onkeydown = function (event) {
+        if (event.code === "Space" || event.code === "Enter") {
+            console.log(event.code)
+            tecla.classList.add("ativa")
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove("ativa")
+    }
+}
