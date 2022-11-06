8319dfe5101f42e0070b0dd5db170ab374842071
Douglas Sergio <douglassergio95@hotmail.com>
2022-11-06 14:33:31 -0300

    atualizacao

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import logo from './img/logo.svg';
+
+function App() {
+  return (
+    <div >
+      <header className='app-header'>
+        <div className='logo'>
+          <img src={logo}></img>
+          Alura-Books
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
